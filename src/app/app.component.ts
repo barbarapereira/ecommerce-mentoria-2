45f16b9e65c4ada8cd1577ce048316ec376d7dc3
Barbara <barbara.pereira1990@gmail.com>
2024-05-06 18:21:20 -0300

    feat (layout): create header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LayoutModule } from '@ecommerce-mentoria-2/layout';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  imports: [NxWelcomeComponent, RouterModule, LayoutModule],
+  selector: 'ecommerce-mentoria-2-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
